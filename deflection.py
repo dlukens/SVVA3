89ab5fc37779243a98eb24cc44d076ddf81316eb
sartajsingh18 <sartajsingh18.09@gmail.com>
2020-02-17 17:34:33 +0100

    created files

---
+++ deflection.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 17 13:54:25 2020
+
+@author: sartaj
+"""
+
---
+++ twist.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 17 15:06:36 2020
+
+@author: sarta
+"""
+
