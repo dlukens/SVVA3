04ba76e2c135530f02a1b27eb35b008198158deb
dlukens <lukensdiego@gmail.com>
2020-02-19 13:31:41 +0100

    Big update, now works

--- interpolationA03.py
+++ interpolationA03.py
@@ -19,81 +19,106 @@ def theta(i,N):
     t =  math.pi*(i-1)/N
     return t
 
+#Grid on chord - Z-Axis
 for i in range(1, Nz+1):
     data_z[i-1] = -Ca/4*(2 - math.cos(theta(i, Nz)) - math.cos(theta(i+1, Nz)))
     
-
+#Grid on span - X-Axis
 for i in range(1, Nx+1):
     data_x[i-1] = la/4*(2 - math.cos(theta(i, Nx)) - math.cos(theta(i+1, Nx)))
 
 ######### interpolation ##########
 
-#new 2x81 matrix of data_z and aero force 
-aeroforce_z = np.array([data_z,data.T[0,:]])
+aeroforce_z = data.T
+aeroforce_x = data
 
 
-n = len(data_z)-1
-a = np.zeros(n)
-b = np.zeros(n)
-c = np.zeros(n)
-d = aeroforce_z[1,:]                                                       #d variable solved
-h = np.zeros(n)
+def interpolate(force, grid):
+    coeff = np.zeros((len(force)-1, 4, len(force.T)-1))
 
+    for r in range(len(force)-1):
 
-#define variable function for h, dependent on z data              
-for i in range(0,n):           
-    h[i] = (data_z[i+1] - data_z[i])
+        n = len(grid)-1
+        h = np.zeros(n)
+        a = np.zeros(n)
+        b = np.zeros(n)
+        c = np.zeros(n)
+        d = force[r,:] #d variable solved
+        
+        
+        for i in range(0,n):           
+            h[i] = (grid[i+1] - grid[i])
+        
+        #create matrix A
+        A = np.zeros((n+1, n+1))
+        
+        #non-zero corners of matrix A (top left, bottom right)
+        A[0,0] = 1
+        A[n,n] = 1
+        
+        #rest of non-zero values of matrix A
+        for j in range(1,n):
+            A[j,j] = (h[j-1] + h[j])/3
+            A[j,j-1] = h[j-1]/6
+            A[j,j+1] = h[j]/6
+            
+        #create v_matrix
+        v = np.zeros(n+1)  
+            
+        for k in range(0,n-1):
+            v[k+1] = 3*((d[k+2]-d[k+1])/h[k+1] - (d[k+1]-d[k])/h[k])
+        
+        #solve A*b = v
+        #b are the values M(0) --> M(n)
+        b = np.linalg.solve(A, v)
+        
+        #Solve for a and c   
+        for l in range(0, n):
+            c[l] = (d[l+1]-d[l])/h[l] - h[l]*(2*b[l]+b[l+1])/3        #c variable solved
+            a[l] = (b[l+1] - b[l])/(3*h[l])                           #a variable solved
+            
+        #Remove last element to have 80 intervals
+        b = b[:-1]
+        d = d[:-1]
+        
+        #Now 3D array with all coefficients for each section and each chord
+            #Index 0: Spanwise chord section - along X-axis
+            #Index 1: Coefficient value (a, b, c, d)
+            #Index 2: Chordwise interval - along -Z-Axis
+        
+        coeff[r, 0, :] = a
+        coeff[r, 1, :] = b
+        coeff[r, 2, :] = c
+        coeff[r, 3, :] = d
+        
+      
+    return coeff
 
-#create matrix A
-A = np.zeros((n+1, n+1))
+def plotinterp(delta, grid, coeff):
+    #Use negative step size
+    n = len(grid)-1
 
-#non-zero corners of matrix A (top left, bottom right)
-A[0,0] = 1
-A[n,n] = 1
-
-#rest of non-zero values of matrix A
-for j in range(1,n):
-    A[j,j] = (h[j-1] + h[j])/3                               #values along diagonal of matrix
-    A[j,j-1] = h[j-1]/6                                                #values parallell to diagonal below diagonal
-    A[j,j+1] = h[j]/6                                           #values parallel to diagonal above diagonal
+    S = []
+    z = []
     
-#create v_matrix
-v = np.zeros(n+1)  
+    #S(x) = a(z-z_i)^3 + b(z-z_i)^2 + c(z-z_i) + d
     
-for k in range(0,n-1):
-    v[k+1] = 3*((d[k+2]-d[k+1])/h[k+1] - (d[k+1]-d[k])/h[k])
+    for t in range(0,n-1):
+        for u in np.arange(grid[t], grid[t+1], delta):
+            S.append(coeff[0, t]*(u - grid[t])**3 + coeff[1, t]*(u - grid[t])**2 + coeff[2, t]*(u - grid[t]) + coeff[3, t])
+            z.append(u)
+            
+    plt.plot(z, S)
+    plt.show()
+            
 
-#solve A*b = v (where A, b and v are matrices)
-#b are the values values M(0) --> M(n)
-b = np.linalg.solve(A, v)                                           #b variable solved
+coeff_z = interpolate(aeroforce_z, data_z)
+coeff_x = interpolate(aeroforce_x, data_x)
 
-#d = y values of data_z 
-#calculate other coefficients         
-     
-for l in range(0, n-1):
-    c[l] = (d[l+1]-d[l])/h[l] - h[l]*(2*b[l]+b[l+1])/3        #c variable solved
-    a[l] = (b[l+1] - b[l])/(3*h[l])                           #a variable solved
 
-    
-#make a plot of interpolation
-#step size, step size is negative as x coordinates become increasingly negative towards TE
-#if step size becomes smaller than -0.01, function becomes unbounded
-delta = -0.0001                                           
-S = []
-z = []
+plotinterp(0.0001, data_x, coeff_x[5,:,:])
+plotinterp(-0.0001, data_z, coeff_z[5,:,:])
 
-#S(x) = a(z-z_i)^3 + b(z-z_i)^2 + c(z-z_i) + d
-
-for t in range(0,n-1):
-    for u in np.arange(data_z[t], data_z[t+1], delta):
-        S.append(a[t]*(u - data_z[t])**3 + b[t]*(u - data_z[t])**2 + c[t]*(u - data_z[t]) + d[t])
-        z.append(u)
-    
-    
-print(a, b, c, d)
-
-plt.plot(z, S)
-plt.show()
 
 
     
