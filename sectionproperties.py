90e037b28d2bad41da686a3ea07225dbd2843a0f
ElliottDesmit <49939835+ElliottDesmit@users.noreply.github.com>
2020-02-27 03:10:42 +0100

    shear flows added, shear center updated

--- sectionproperties.py
+++ sectionproperties.py
@@ -101,53 +101,11 @@ Iyy += Ast*Cz**2 + 2*(Iyy_ss + Ad_yss) + Iyy_sp + Iyy_arc +Ad_yarc # [m^4]
 #print(Iyy)
 
 # Shear Center
+# calculated numerically in separate file
+from shearcenter import SCz, eta
 
-Sy = 1 # [N] unit shear load
-
-# base shear flows
-q1 = -(Sy*Tsk*h**2)/(4*Izz) - (Sy*Ast*y_st1)/Izz  # [N/m]
-q2 = -(Sy*Tsp*h**2)/(8*Izz) # [N/m]
-q3 = -(Sy*h*Lssk*Tsk)/(4*Izz) - ((Sy*Ast)/Izz)*(y_st2+y_st3+y_st4+y_st5) + q1 + q2 # [N/m]
-q4 = (Sy*h*Lssk*Tsk)/(4*Izz) + ((Sy*Ast)/Izz)*(y_st2+y_st3+y_st4+y_st5) + q3 # [N/m] 
-q5 = (Sy*Tsp*h**2)/(8*Izz) + q4 # [N/m]
-q6 = (Sy*Tsk*h**2)/(4*Izz) + (Sy*Ast*y_st1)/Izz + q4
-
-# distances along which the boom shears act
-e1 = Lssk - 2*delta_str + m.pi*h/4 # length of straight skin between boom 2 and the TE
-e2 = 5*delta_str - m.pi*h/4 # length of straight skin between boom 5 and the start of the arc
-E1 = np.array([e1, e1 - delta_str, e1 - 2*delta_str, e1 - 3*delta_str])
-E2 = np.array([e2 - 3*delta_str, e2 - 2*delta_str, e2 - delta_str, e2])
-
-# base shear forces
-S1 = (Sy*Tsk*(h**3)*(1 - m.pi/2))/(8*Izz) - (Sy*Ast*y_st1*(h*m.pi/4 - delta_str))/Izz # [N]
-S2 = -(Sy*Tsp*h**3)/(16*Izz) # [N]
-S3 = -(Sy*Tsk*h*Lssk**2)/(6*Izz) - ((Sy*Ast)/(Izz))*(y_st2*E1[0]+y_st3*E1[1]+y_st4*E1[2]+y_st5*E1[3]) + (q1 + q2)*Lssk # [N]
-S4 = (Sy*Tsk*h*Lssk**2)/(12*Izz) + ((Sy*Ast)/(Izz))*(y_st2*E2[0]+y_st3*E2[1]+y_st4*E2[2]+y_st5*E2[3]) + q3*Lssk # [N]
-S5 = -(Sy*Tsp*h**3)/(12*Izz) + q4*h/2 # [N]
-S6 = -(Sy*Tsk*h**2)/(4*Izz) + (Sy*Ast*y_st1*delta_str)/(Izz) + q4*h*m.pi/4
-
-# redundant shear flows: clockwise positive
-A = np.zeros((2,2))
-b = np.zeros((2,1))
-a1, a2, b1 = ((m.pi*h)/(2*Tsk) + h/Tsp), -h/Tsp, -(S1 + S6)/Tsk + (S2 + S5)/Tsp
-a3, a4, b2 = -h/Tsp, (h/Tsp + (2*Lssk)/Tsk), -(S3 + S4)/Tsk - (S2 + S5)/Tsp
-A[0,:] = a1, a2
-A[1,:] = a3, a4
-b[0,:] = b1
-b[1,:] = b2
-
-# results of redundant shear flows
-Y = np.linalg.solve(A,b)
-q0_1, q0_2 = Y[0,0], Y[1,0]
-
-# taking moment about midpoint of the spar and clockwise positive
-A1 = 0.5*m.pi*(h/2)**2 # area of cell 1
-A2 = (Ca - h/2)*h/2 # area of cell 2
-d = h*m.cos(angle_Lssk)/2 # moment arm for q23 and q31
-Mi = (h/2)*(S1 + S6) + d*(S3 + S4) + 2*(A1*q0_1 + A2*q0_2)
-
-Ksi = -Mi/Sy # distance midpoint spar - shear center (positive)
-SCz = -h/2 - Ksi # shear center [m]
+eta = eta
+SCz = SCz
 #print(SCz)
 
 # Torsional Stiffness
